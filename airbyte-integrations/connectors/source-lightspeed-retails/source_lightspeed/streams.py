6625740f1ed3a15af61d3d564a008179938ab717
Nicholas Joanisse <nickjoanis@Nicholass-MacBook-Pro.local>
2022-12-12 16:00:06 -0500

    lightspeed-retail - Makefile and integration testing

--- airbyte-integrations/connectors/source-lightspeed-retails/source_lightspeed/streams.py
+++ airbyte-integrations/connectors/source-lightspeed-retails/source_lightspeed/streams.py
@@ -10,6 +10,7 @@ from airbyte_cdk.sources.streams.http import HttpStream
 
 import dateutil.parser as parser
 
+
 class LightspeedStream(HttpStream, ABC):
 
     def __init__(self, config: Dict):
@@ -53,12 +54,13 @@ class LightspeedStream(HttpStream, ABC):
             params['load_relations'] = self.relationships
         return params
 
-
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        results = response.json().get(self.data_field, []) if self.data_field is not None else response.json()
+        results = response.json().get(
+            self.data_field, []) if self.data_field is not None else response.json()
         records = results if isinstance(results, list) else [results]
         for record in records:
             yield record
+
 
 class IncrementalLightspeedStream(LightspeedStream, ABC):
 
@@ -69,125 +71,140 @@ class IncrementalLightspeedStream(LightspeedStream, ABC):
         return {self.cursor_field: max(latest_record.get(self.cursor_field, ""), current_stream_state.get(self.cursor_field, ""))}
 
     def request_params(self, stream_state: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None, **kwargs):
-        params = super().request_params(stream_state=stream_state, next_page_token=next_page_token, **kwargs) or {}
+        params = super().request_params(stream_state=stream_state,
+                                        next_page_token=next_page_token, **kwargs) or {}
         params['orderby'] = self.cursor_field
         if not next_page_token:
             if stream_state:
                 params[self.cursor_field] = f">,{parser.parse(stream_state.get(self.cursor_field))}"
         return params
 
-class Categories(LightspeedStream):
+
+class Categories(IncrementalLightspeedStream):
     """
     API docs: https://developers.lightspeedhq.com/retail/endpoints/Category/
     """
-    
+
     data_field = "Category"
     primary_key = "categoryID"
 
-class Customers(LightspeedStream):
+
+class Customers(IncrementalLightspeedStream):
     """
     API docs: https://developers.lightspeedhq.com/retail/endpoints/Customer/
     """
-    
+
     data_field = "Customer"
     primary_key = "customerID"
 
     relationships = '["Contact"]'
 
-class Discounts(LightspeedStream):
+
+class Discounts(IncrementalLightspeedStream):
     """
     API docs: https://developers.lightspeedhq.com/retail/endpoints/Discount/
     """
-    
+
     data_field = "Discount"
     primary_key = "discountID"
+
 
 class Items(IncrementalLightspeedStream):
     """
     API docs: https://developers.lightspeedhq.com/retail/endpoints/Item/
     """
-    
+
     data_field = "Item"
     primary_key = "itemID"
 
     relationships = '["ItemShops","ItemPrices"]'
 
-class ItemMatrices(LightspeedStream):
+
+class ItemMatrices(IncrementalLightspeedStream):
     """
     API docs: https://developers.lightspeedhq.com/retail/endpoints/ItemMatrix/
     """
-    
+
     data_field = "ItemMatrix"
     primary_key = "itemMatrixID"
 
-class ItemAttributeSets(LightspeedStream):
+
+class ItemAttributeSets(IncrementalLightspeedStream):
     """
     API docs: https://developers.lightspeedhq.com/retail/endpoints/ItemAttributeSet/
     """
-    
-    data_field = "ItemAttributeSet"
-    primary_key = "ItemAttributeSetID"
 
-class Manufacturers(LightspeedStream):
+    data_field = "ItemAttributeSet"
+    primary_key = "itemAttributeSetID"
+
+
+class Manufacturers(IncrementalLightspeedStream):
     """
     API docs: https://developers.lightspeedhq.com/retail/endpoints/Manufacturer/
     """
-    
+
     data_field = "Manufacturer"
     primary_key = "manufacturerID"
+
 
 class PaymentTypes(LightspeedStream):
     """
     API docs: https://developers.lightspeedhq.com/retail/endpoints/PaymentType/
     """
-    
+
     data_field = "PaymentType"
     primary_key = "paymentTypeID"
-    
+
+
 class Sales(IncrementalLightspeedStream):
     """
     API docs: https://developers.lightspeedhq.com/retail/endpoints/Sale/
     """
-    
+
     data_field = "Sale"
     primary_key = "saleID"
-    
+
+
 class SaleLines(IncrementalLightspeedStream):
     """
     API docs: https://developers.lightspeedhq.com/retail/endpoints/SaleLine/
     """
-    
+
     data_field = "SaleLine"
     primary_key = "saleID"
 
-class SalePayments(LightspeedStream):
+
+class SalePayments(IncrementalLightspeedStream):
     """
     API docs: https://developers.lightspeedhq.com/retail/endpoints/SalePayment/
     """
-    
+
     data_field = "SalePayment"
     primary_key = "salePaymentID"
 
-class Shops(LightspeedStream):
+
+class Shops(IncrementalLightspeedStream):
     """
     API docs: https://developers.lightspeedhq.com/retail/endpoints/Shop/
     """
-    
+
     data_field = "Shop"
     primary_key = "shopID"
 
-class TaxCategories(LightspeedStream):
+
+class TaxCategories(IncrementalLightspeedStream):
     """
     API docs: https://developers.lightspeedhq.com/retail/endpoints/TaxCategory/
     """
-    
+
     data_field = "TaxCategory"
     primary_key = "taxCategoryID"
 
-class TaxClasses(LightspeedStream):
+
+class TaxClasses(IncrementalLightspeedStream):
     """
     API docs: https://developers.lightspeedhq.com/retail/endpoints/TaxClass/
     """
-    
+
     data_field = "TaxClass"
     primary_key = "taxClassID"
