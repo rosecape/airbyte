bb3ab775ca215c1715d9f75bd23b97c03dd0f44b
Nicholas Joanisse <nickjoanis@Nicholass-MacBook-Pro.local>
2022-12-14 12:58:07 -0500

    booker - [WIP] reworked streams to proper inheritance to leverage Airybtes built-in functions and deduplicate code

--- airbyte-integrations/connectors/source-booker/source_booker/streams.py
+++ airbyte-integrations/connectors/source-booker/source_booker/streams.py
@@ -48,19 +48,20 @@ class BookerStream(HttpStream, ABC):
 
 class IncrementalBookerStream(BookerStream, ABC):
 
-    def _chunk_date_range(self, start_date: datetime) -> List[Mapping[str, any]]:
-        dates = []
-        while start_date < (datetime.now() - timedelta(days=1)):
-            dates.append({self.cursor_field: start_date.strftime("%Y-%m-%d")})
-            start_date += timedelta(days=1)
-        return dates
+    def __init__(self, config: Mapping[str, Any], authenticator: BookerAuthenticator):
+        super().__init__(config, authenticator)
+        self._cursor_value = None
+    
+    @property
+    def state(self) -> Mapping[str, Any]:
+        if self._cursor_value:
+            return {self.cursor_field: self._cursor_value.strftime('%Y-%m-%d')}
+        else:
+            return {self.cursor_field: self.start_date.strftime('%Y-%m-%d')}
 
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        start_date = datetime.strptime(stream_state[self.cursor_field], "%Y-%m-%d") if stream_state and self.cursor_field in stream_state else datetime.strptime(self.config["start_date"], "%Y-%m-%d")
-        return self._chunk_date_range(start_date)
-
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        return {self.cursor_field: max(latest_record.get(self.cursor_field, ""), current_stream_state.get(self.cursor_field, ""))}
+    @state.setter
+    def state(self, value: Mapping[str, Any]):
+        self._cursor_value = datetime.strptime(value[self.cursor_field], '%Y-%m-%d')
         
 class Treatments(BookerStream):
     
@@ -77,15 +78,36 @@ class Appointments(IncrementalBookerStream):
 
     def path(self, **kwargs) -> str:
         return "appointments"
-        
+
     http_method = "POST"
     
     primary_key = "ID"
     data_field = "Results"
-    cursor_field = "DateLastModified"
+    cursor_field = "StartDateTimeOffset"
 
     def request_body_json(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None,) -> Optional[Mapping]:
-        data = super().request_body_json(stream_state, stream_slice, next_page_token)
-        data["DateStart"] = stream_slice[self.cursor_field]
-        data["DateEnd"] = stream_slice[self.cursor_field]
-        return data
+        print(self._cursor_value)
+        super_data = super().request_body_json(stream_state, stream_slice, next_page_token)
+        data = {
+            "FromStartDateOffset": """{}T00:00:00-0400""".format(stream_slice[self.cursor_field]),
+            "ToStartDateOffset": """{}T00:00:00-0400""".format((datetime.strptime(stream_slice[self.cursor_field], '%Y-%m-%d') + timedelta(days=1)).strftime('%Y-%m-%d')),
+        }
+        return {**super_data, **data}
+
+    def _chunk_date_range(self, start_date: datetime) -> List[Mapping[str, any]]:
+        dates = []
+        while start_date < (datetime.now() - timedelta(days=1)):
+            dates.append({self.cursor_field: start_date.strftime("%Y-%m-%d")})
+            start_date += timedelta(days=1)
+        return dates
+
+    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
+        start_date = datetime.strptime(stream_state[self.cursor_field], "%Y-%m-%d") if stream_state and self.cursor_field in stream_state else datetime.strptime(self.config["start_date"], "%Y-%m-%d")
+        return self._chunk_date_range(start_date)
+
+    def read_records(self, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
+        for record in super().read_records(*args, **kwargs):
+            if self._cursor_value:
+                latest_record_date = datetime.strptime(record[self.cursor_field], '%Y-%m-%d')
+                self._cursor_value = max(self._cursor_value, latest_record_date)
+            yield record
