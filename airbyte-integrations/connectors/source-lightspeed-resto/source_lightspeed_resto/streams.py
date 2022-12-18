089adda714be6825eb73bb603d01c8c105a658d6
Nicholas Joanisse <nickjoanis@Nicholass-MacBook-Pro.local>
2022-12-18 13:40:22 -0500

    resto - [WIP] Authentication method (legacy) passed as token to request_auth_header

--- airbyte-integrations/connectors/source-lightspeed-resto/source_lightspeed_resto/streams.py
+++ airbyte-integrations/connectors/source-lightspeed-resto/source_lightspeed_resto/streams.py
@@ -12,6 +12,8 @@ from datetime import datetime, timedelta, timezone
 from airbyte_cdk.sources.streams.http import HttpStream
 from source_lightspeed_resto.auth import LightspeedRestoAuthenticator
 
+from airbyte_cdk.sources.streams import IncrementalMixin
+
 
 class LightspeedRestoStream(HttpStream, ABC):
 
@@ -22,7 +24,7 @@ class LightspeedRestoStream(HttpStream, ABC):
 
     @property
     def url_base(self) -> str:
-        return "https://staging-integration.posios.com/PosServer/rest/"
+        return self.config['url'] if self.config['production'] else "https://staging-integration.posios.com/PosServer/rest/"
 
     def request_headers(self, **kwargs) -> Mapping[str, Any]:
         return self._authenticator.get_auth_header()
@@ -43,16 +45,17 @@ class LightspeedRestoStream(HttpStream, ABC):
         return params
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        print(response.url)
         return response.json().get('results') if 'results' in response.json() else response.json()
 
 
-class IncrementalLightspeedRestoStream(LightspeedRestoStream, ABC):
+class IncrementalLightspeedRestoStream(LightspeedRestoStream, IncrementalMixin):
 
     def __init__(self, config: Mapping[str, Any], authenticator: LightspeedRestoAuthenticator):
         super().__init__(config, authenticator)
         self._cursor_value = None
 
-    # order_field = "timeStamp"
+    order_field = "modificationDate"
     cursor_field = "modificationDate"
     
     @property
@@ -64,7 +67,7 @@ class IncrementalLightspeedRestoStream(LightspeedRestoStream, ABC):
     
     @state.setter
     def state(self, value: Mapping[str, Any]):
-       self._cursor_value = datetime.strptime(value[self.cursor_field], '%Y-%m-%d')
+        self._cursor_value = value[self.cursor_field]
 
     def _chunk_date_range(self, start_date: datetime) -> List[Mapping[str, any]]:
         dates = []
@@ -72,38 +75,37 @@ class IncrementalLightspeedRestoStream(LightspeedRestoStream, ABC):
             dates.append({self.cursor_field: start_date.strftime("%Y-%m-%d")})
             start_date += timedelta(days=1)
         return dates
-
+        
     def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        start_date = datetime.strptime(stream_state[self.cursor_field], "%Y-%m-%d") if stream_state and self.cursor_field in stream_state else datetime.strptime(self.config["start_date"], "%Y-%m-%d")
-        return self._chunk_date_range(start_date)
+        start_date = stream_state[self.cursor_field] if stream_state and self.cursor_field in stream_state else self.config["start_date"]
+        return self._chunk_date_range(datetime.strptime(start_date, '%Y-%m-%d'))
 
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        return {self.cursor_field: max(latest_record.get(self.cursor_field, ""), current_stream_state.get(self.cursor_field, ""))}
-
-    def request_params(self, stream_state: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None, **kwargs):
+    def request_params(self, stream_slice, stream_state: Mapping[str, Any], next_page_token: Mapping[str, Any], **kwargs):
         params = super().request_params(stream_state=stream_state,
                                         next_page_token=next_page_token, **kwargs) or {}
         params['useModification'] = "true"
         params['orderby'] = self.cursor_field
-        # if not next_page_token:
-        #     if stream_state:
-        params[self.cursor_field] = self.config['start_date']
+
+        if not next_page_token:
+            params['from'] = self._cursor_value or stream_slice[self.cursor_field]
+            params['to'] = (datetime.strptime(params['from'], '%Y-%m-%d') + timedelta(days=1)).strftime("%Y-%m-%d")
+        
         return params
 
     def read_records(self, stream_state: Mapping[str, Any] = None, stream_slice: Optional[Mapping[str, Any]] = None, **kwargs) -> Iterable[Mapping[str, Any]]:
         records = super().read_records(stream_slice=stream_slice, **kwargs)
         if next(records, object()) is not object():
-            for record in super().read_records(stream_slice=stream_slice, **kwargs):
-                cursor_value = self._cursor_value.replace(tzinfo=timezone.utc)
-                latest_record_date = datetime.strptime(record[self.cursor_field], '%Y-%m-%dT%H:%M:%S%z').replace(tzinfo=timezone.utc)
-                self._cursor_value = max(cursor_value, latest_record_date) if cursor_value else latest_record_date
+            for record in records:
+                cursor_value = self._cursor_value
+                latest_record_date = record[self.cursor_field]
+                self._cursor_value = max(datetime.strptime(cursor_value, '%Y-%m-%d'), datetime.strptime(latest_record_date, '%Y-%m-%dT%H:%M:%S.%fZ')).strftime("%Y-%m-%d") if cursor_value else latest_record_date
                 yield record
-        self._cursor_value = datetime.strptime(stream_slice[self.cursor_field], '%Y-%m-%d').replace(tzinfo=timezone.utc)
+        self._cursor_value = stream_slice[self.cursor_field]
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         response = response.json()
         offset = int(response.get('offset')) + int(response.get('amount'))
-        if offset < int(response.get('total')):
+        if 'total' in response and offset < int(response.get('total')):
             return offset
         return None
 
@@ -137,6 +139,3 @@ class Receipts(IncrementalLightspeedRestoStream):
     
     def path(self, **kwargs) -> str:
         return "financial/receipt"
-
-    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        return None
