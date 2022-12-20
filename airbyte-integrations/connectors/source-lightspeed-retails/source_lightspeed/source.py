0f847cfaa7afce7845b12252eda8a7af287727fd
Nicholas Joanisse <nickjoanis@ip-192-168-2-53.ca-central-1.compute.internal>
2022-12-19 22:16:51 -0500

    retails - [WIP] moved from custom authenticator to Oauth2 authenticator

--- airbyte-integrations/connectors/source-lightspeed-retails/source_lightspeed/source.py
+++ airbyte-integrations/connectors/source-lightspeed-retails/source_lightspeed/source.py
@@ -6,9 +6,8 @@ from typing import Any, List, Mapping, Tuple
 
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+from airbyte_cdk.sources.streams.http.auth import Oauth2Authenticator
 
-from .auth import LightspeedAuthenticator
 from source_lightspeed.streams import (
     Categories,
     Customers,
@@ -27,24 +26,33 @@ from source_lightspeed.streams import (
 )
 class SourceLightspeed(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
-        LightspeedAuthenticator(config).authentication()
+        authenticator = Oauth2Authenticator(
+            token_refresh_endpoint="https://api.lightspeedapp.com/oauth/access_token.php",
+            client_id=config["client_id"],
+            client_secret=config["client_secret"],
+            refresh_token=config["refresh_token"]
+        )
         return True, None
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
-        token = LightspeedAuthenticator(config).authentication()
-        config['authenticator'] = TokenAuthenticator(token=token)
+        authenticator = Oauth2Authenticator(
+            token_refresh_endpoint="https://api.lightspeedapp.com/oauth/access_token.php",
+            client_id=config["client_id"],
+            client_secret=config["client_secret"],
+            refresh_token=config["refresh_token"]
+        )
         return [
-            Categories(config),
-            Customers(config),
-            Discounts(config),
-            Items(config),
-            ItemAttributeSets(config),
-            PaymentTypes(config),
-            Manufacturers(config),
-            Sales(config),
-            SaleLines(config),
-            SalePayments(config),
-            Shops(config),
-            TaxCategories(config),
-            TaxClasses(config)
+            Categories(config, authenticator),
+            Customers(config , authenticator),
+            Discounts(config, authenticator),
+            Items(config, authenticator),
+            ItemAttributeSets(config, authenticator),
+            PaymentTypes(config, authenticator),
+            Manufacturers(config, authenticator),
+            Sales(config, authenticator),
+            SaleLines(config, authenticator),
+            SalePayments(config, authenticator),
+            Shops(config, authenticator),
+            TaxCategories(config, authenticator),
+            TaxClasses(config, authenticator),
         ]
