57a394d1a2ea2b645a97e0a40484d58d1bb6c968
Nicholas Joanisse <nickjoanis@ip-192-168-0-121.ca-central-1.compute.internal>
2022-12-21 15:52:49 -0500

    resto - changed the dates format to match the API specs

--- airbyte-integrations/connectors/source-lightspeed-resto/source_lightspeed_resto/streams.py
+++ airbyte-integrations/connectors/source-lightspeed-resto/source_lightspeed_resto/streams.py
@@ -9,6 +9,8 @@ import requests
 import dateutil.parser as parser
 from datetime import datetime, timedelta, timezone
 
+from airbyte_cdk.models import SyncMode
+
 from airbyte_cdk.sources.streams.http import HttpStream
 from source_lightspeed_resto.auth import LightspeedRestoAuthenticator
 
@@ -53,20 +55,20 @@ class IncrementalLightspeedRestoStream(LightspeedRestoStream, IncrementalMixin):
     def __init__(self, config: Mapping[str, Any], authenticator: LightspeedRestoAuthenticator):
         super().__init__(config, authenticator)
         self._cursor_value = None
-
-    order_field = "modificationDate"
-    cursor_field = "modificationDate"
     
     @property
     def state(self) -> Mapping[str, Any]:
         if self._cursor_value:
-            return {self.cursor_field: self._cursor_value.strftime('%Y-%m-%dT%H:%M:%S.%fZ')}
+            return {self.cursor_field: self._cursor_value}
         else:
             return {self.cursor_field: self.config['start_date']}
     
     @state.setter
     def state(self, value: Mapping[str, Any]):
         self._cursor_value = value[self.cursor_field]
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]):
+        return self.state
 
     def _chunk_date_range(self, start_date: datetime) -> List[Mapping[str, any]]:
         dates = []
@@ -79,22 +81,26 @@ class IncrementalLightspeedRestoStream(LightspeedRestoStream, IncrementalMixin):
         start_date = stream_state[self.cursor_field] if stream_state and self.cursor_field in stream_state else self.config["start_date"]
         return self._chunk_date_range(datetime.strptime(start_date, '%Y-%m-%dT%H:%M:%S.%fZ'))
 
-    def read_records(self, stream_state: Mapping[str, Any] = None, stream_slice: Optional[Mapping[str, Any]] = None, **kwargs) -> Iterable[Mapping[str, Any]]:
-        records = super().read_records(stream_slice=stream_slice, **kwargs)
-        if next(records, object()) is not object():
-            for record in records:
-                cursor_value = self._cursor_value
-                latest_record_date = record[self.cursor_field]
-                self._cursor_value = max(cursor_value, latest_record_date) if cursor_value else latest_record_date
-                yield record
-        self._cursor_value = stream_slice[self.cursor_field]
+    def read_records(
+        self,
+        sync_mode: SyncMode,
+        cursor_field: List[str] = None,
+        stream_slice: Mapping[str, Any] = None,
+        stream_state: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping[str, Any]]:
+
+        records = super().read_records(sync_mode=sync_mode, cursor_field=cursor_field, stream_slice=stream_slice, stream_state=stream_state)
+        for record in records:
+            next_cursor_value = record[self.cursor_field]
+            self._cursor_value = max(self._cursor_value, next_cursor_value) if self._cursor_value else next_cursor_value
+            yield record
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         response = response.json()
+        if len(response.get('results')) == 0:
+            return None
         offset = int(response.get('offset')) + int(response.get('amount'))
-        if 'total' in response and offset < int(response.get('total')):
-            return offset
-        return None
+        return offset
 
 
 class Customers(LightspeedRestoStream):
@@ -102,7 +108,6 @@ class Customers(LightspeedRestoStream):
     API docs: https://developers.lightspeedhq.com/resto-api/endpoints/corecustomer
     """
     primary_key = "id"
-    cursor_field = "modifiedSince"
     
     def path(self, **kwargs) -> str:
         return "core/customer"
@@ -124,6 +129,8 @@ class Receipts(IncrementalLightspeedRestoStream):
     API docs: https://developers.lightspeedhq.com/resto-api/endpoints/financial/#get-all-receipts-for-a-certain-date-or-date-range
     """
     primary_key = "id"
+    cursor_field = "modificationDate"
+    order_field = "modificationDate"
     
     def path(self, **kwargs) -> str:
         return "financial/receipt"
@@ -131,11 +138,11 @@ class Receipts(IncrementalLightspeedRestoStream):
     def request_params(self, stream_slice, stream_state: Mapping[str, Any], next_page_token: Mapping[str, Any], **kwargs):
         params = super().request_params(stream_state=stream_state,
                                         next_page_token=next_page_token, **kwargs) or {}
+        
         params['useModification'] = "true"
         params['orderby'] = self.cursor_field
+        params['offset'] = next_page_token or 0
 
-        if not next_page_token:
-            params['from'] = self._cursor_value or stream_slice[self.cursor_field]
-            params['to'] = (datetime.strptime(params['from'], '%Y-%m-%dT%H:%M:%S.%fZ') + timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-        
+        params['date'] = datetime.strptime(stream_slice[self.cursor_field], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y-%m-%d')
+
         return params
